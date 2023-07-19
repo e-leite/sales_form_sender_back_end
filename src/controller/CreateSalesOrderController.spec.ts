225a010b022c27e8fdccbc4da764145b0abb0c45
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-07-19 06:42:49 -0300

    test: ensure CreateSalesOrderController return status code 400 if invoiceDate is null

--- src/controller/CreateSalesOrderController.spec.ts
+++ src/controller/CreateSalesOrderController.spec.ts
@@ -76,4 +76,39 @@ describe("CreateSalesOrderController", () => {
     const response = sut.handle(httpRequest);
     expect(response?.statusCode).toBe(400);
   });
+
+  test("Should return status code 400 if invoiceDate is null.", () => {
+    const sut = new CreateSalesOrderController();
+
+    const httpRequest: HttpRequest<InputCreateSalesOrderDto> = {
+      body: {
+        salesDate: new Date(Date.now()),
+        invoiceDate: null,
+        userId: 1,
+        userName: "any_user",
+        userEmail: "any_email",
+        managerName: "any_manager",
+        managerEmail: "anay_email",
+        customerId: 1,
+        customerName: "any_customer",
+        customerCity: "any_city",
+        customerState: "any_state",
+        customerPaymentTerm: "any_term",
+        shipBase: "any_base",
+        shipmentType: "any_type",
+        shippingCompanyName: null,
+        shippingCompanyContact: null,
+        shippingCompanyPhone: null,
+        shippingCompanyEmail: null,
+        mapsLink: null,
+        addressHasUnpavedRoad: true,
+        unpavedRoadSize: null,
+        shippingNote: null,
+        status: "string",
+      },
+    };
+
+    const response = sut.handle(httpRequest);
+    expect(response?.statusCode).toBe(400);
+  });
 });
