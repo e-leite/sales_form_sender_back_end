f03ad82751ae0a865fc4d166d09bf75695eaad6d
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-08-07 12:52:53 -0300

    test: ensures CreateSalesOrderController return status code 400 if customerId is null

--- src/controller/CreateSalesOrderController.spec.ts
+++ src/controller/CreateSalesOrderController.spec.ts
@@ -33,6 +33,7 @@ class CreateSalesOrderController implements IController {
       "userEmail",
       "managerName",
       "managerEmail",
+      "customerId",
     ];
     const requiredFieldsTranslated = [
       "Data do pedido",
@@ -42,6 +43,7 @@ class CreateSalesOrderController implements IController {
       "Email do usuário",
       "Nome do Gerente",
       "Email do Gerente",
+      "Id do cliente",
     ];
 
     for (const field of requiredFields) {
@@ -361,5 +363,42 @@ describe("CreateSalesOrderController", () => {
 
     expect(response?.statusCode).toBe(400);
     expect(response?.body).toBe("O campo Email do Gerente não pode ser nulo.");
+  });
+
+  test("Should return status code 400 if customerId is null", async () => {
+    const sut = createSut();
+
+    const httpRequest = {
+      body: {
+        salesDate: "2023-08-01",
+        invoiceDate: "2023-08-05",
+        userId: 1,
+        userName: "any_name",
+        userEmail: "any_name",
+        managerName: "any_name",
+        managerEmail: "any_name",
+        customerId: null,
+        customerName: "any_customer",
+        customerCity: "any_city",
+        customerState: "any_state",
+        customerPaymentTerm: "any_term",
+        shipBase: "any_base",
+        shipmentType: "any_type",
+        shippingCompanyName: null,
+        shippingCompanyContact: null,
+        shippingCompanyPhone: null,
+        shippingCompanyEmail: null,
+        mapsLink: null,
+        addressHasUnpavedRoad: true,
+        unpavedRoadSize: null,
+        shippingNote: null,
+        status: "string",
+      },
+    };
+
+    const response = await sut.handle(httpRequest);
+
+    expect(response?.statusCode).toBe(400);
+    expect(response?.body).toBe("O campo Id do cliente não pode ser nulo.");
   });
 });
