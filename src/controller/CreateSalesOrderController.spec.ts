b95ced8626bc821118ce8a20ef7cf142b9733957
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-08-07 12:47:45 -0300

    test: ensures CreateSalesOrderController return status code 400 if managerEmail is null

--- src/controller/CreateSalesOrderController.spec.ts
+++ src/controller/CreateSalesOrderController.spec.ts
@@ -25,12 +25,23 @@ class CreateSalesOrderController implements IController {
     const salesDt = new Date(salesDate);
     const invoiceDt = new Date(invoiceDate);
 
-    const requiredFields = ["salesDate", "invoiceDate", "userId", "userName"];
+    const requiredFields = [
+      "salesDate",
+      "invoiceDate",
+      "userId",
+      "userName",
+      "userEmail",
+      "managerName",
+      "managerEmail",
+    ];
     const requiredFieldsTranslated = [
       "Data do pedido",
       "Data de faturamento",
       "Id do usuário",
       "Nome do usuário",
+      "Email do usuário",
+      "Nome do Gerente",
+      "Email do Gerente",
     ];
 
     for (const field of requiredFields) {
@@ -239,5 +250,116 @@ describe("CreateSalesOrderController", () => {
 
     expect(response?.statusCode).toBe(400);
     expect(response?.body).toBe("O campo Nome do usuário não pode ser nulo.");
+  });
+
+  test("Should return status code 400 if userEmail is null", async () => {
+    const sut = createSut();
+
+    const httpRequest = {
+      body: {
+        salesDate: "2023-08-01",
+        invoiceDate: "2023-08-05",
+        userId: 1,
+        userName: "any_name",
+        userEmail: null,
+        managerName: "any_manager",
+        managerEmail: "anay_email",
+        customerId: 1,
+        customerName: "any_customer",
+        customerCity: "any_city",
+        customerState: "any_state",
+        customerPaymentTerm: "any_term",
+        shipBase: "any_base",
+        shipmentType: "any_type",
+        shippingCompanyName: null,
+        shippingCompanyContact: null,
+        shippingCompanyPhone: null,
+        shippingCompanyEmail: null,
+        mapsLink: null,
+        addressHasUnpavedRoad: true,
+        unpavedRoadSize: null,
+        shippingNote: null,
+        status: "string",
+      },
+    };
+
+    const response = await sut.handle(httpRequest);
+
+    expect(response?.statusCode).toBe(400);
+    expect(response?.body).toBe("O campo Email do usuário não pode ser nulo.");
+  });
+
+  test("Should return status code 400 if managerName is null", async () => {
+    const sut = createSut();
+
+    const httpRequest = {
+      body: {
+        salesDate: "2023-08-01",
+        invoiceDate: "2023-08-05",
+        userId: 1,
+        userName: "any_name",
+        userEmail: "any_name",
+        managerName: null,
+        managerEmail: "anay_email",
+        customerId: 1,
+        customerName: "any_customer",
+        customerCity: "any_city",
+        customerState: "any_state",
+        customerPaymentTerm: "any_term",
+        shipBase: "any_base",
+        shipmentType: "any_type",
+        shippingCompanyName: null,
+        shippingCompanyContact: null,
+        shippingCompanyPhone: null,
+        shippingCompanyEmail: null,
+        mapsLink: null,
+        addressHasUnpavedRoad: true,
+        unpavedRoadSize: null,
+        shippingNote: null,
+        status: "string",
+      },
+    };
+
+    const response = await sut.handle(httpRequest);
+
+    expect(response?.statusCode).toBe(400);
+    expect(response?.body).toBe("O campo Nome do Gerente não pode ser nulo.");
+  });
+
+  test("Should return status code 400 if managerEmail is null", async () => {
+    const sut = createSut();
+
+    const httpRequest = {
+      body: {
+        salesDate: "2023-08-01",
+        invoiceDate: "2023-08-05",
+        userId: 1,
+        userName: "any_name",
+        userEmail: "any_name",
+        managerName: "any_name",
+        managerEmail: null,
+        customerId: 1,
+        customerName: "any_customer",
+        customerCity: "any_city",
+        customerState: "any_state",
+        customerPaymentTerm: "any_term",
+        shipBase: "any_base",
+        shipmentType: "any_type",
+        shippingCompanyName: null,
+        shippingCompanyContact: null,
+        shippingCompanyPhone: null,
+        shippingCompanyEmail: null,
+        mapsLink: null,
+        addressHasUnpavedRoad: true,
+        unpavedRoadSize: null,
+        shippingNote: null,
+        status: "string",
+      },
+    };
+
+    const response = await sut.handle(httpRequest);
+
+    expect(response?.statusCode).toBe(400);
+    expect(response?.body).toBe("O campo Email do Gerente não pode ser nulo.");
   });
 });
