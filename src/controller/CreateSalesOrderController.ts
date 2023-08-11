aac4bf8c309bf80a8f943f68d56b6afc8cedb72b
Emerson Leite <83618300+e-leite@users.noreply.github.com>
2023-08-11 16:42:19 -0300

    feat: move CreateSalesOrderController to CreateSalesOrderController.ts

---
+++ src/controller/CreateSalesOrderController.ts
@@ -0,0 +1,124 @@
+import { SalesOrder } from "../domain/SalesOrder";
+import { ICreateSalesOrderRepository } from "../domain/repository/ICreateSalesOrderRepository";
+import {
+  HttpRequest,
+  HttpResponse,
+  IController,
+  badRequest,
+  created,
+} from "./helpers";
+
+export class CreateSalesOrderController implements IController<SalesOrder> {
+  constructor(
+    private readonly createSalesOrderRepository: ICreateSalesOrderRepository,
+  ) {}
+  async handle(
+    httpRequest: HttpRequest<any>,
+  ): Promise<HttpResponse<SalesOrder | string>> {
+    const { salesDate, invoiceDate, unpavedRoadSize, addressHasUnpavedRoad } =
+      httpRequest.body;
+
+    const salesDt = new Date(salesDate);
+    const invoiceDt = new Date(invoiceDate);
+
+    const requiredFields = [
+      "salesDate",
+      "invoiceDate",
+      "userId",
+      "userName",
+      "userEmail",
+      "userGetFirstPriceRange",
+      "managerName",
+      "managerEmail",
+      "customerId",
+      "customerName",
+      "customerCity",
+      "customerState",
+      "customerPaymentTerm",
+      "shipBase",
+      "shipmentType",
+      "status",
+    ];
+    const requiredFieldsTranslated = [
+      "Data do pedido",
+      "Data de faturamento",
+      "Id do usuário",
+      "Nome do usuário",
+      "Email do usuário",
+      "Usuário recebe primeira faixa de preço",
+      "Nome do Gerente",
+      "Email do Gerente",
+      "Id do cliente",
+      "Nome do cliente",
+      "Cidade do cliente",
+      "Estado do cliente",
+      "Prazo de pagamento",
+      "Base de faturamento",
+      "Tipo de frete",
+      "Status",
+    ];
+
+    for (const field of requiredFields) {
+      if (httpRequest.body?.[field] === null) {
+        const indexOfField = requiredFields.indexOf(field);
+        const translatedField = requiredFieldsTranslated[indexOfField];
+
+        return badRequest(`O campo ${translatedField} não pode ser nulo.`);
+      }
+    }
+
+    if (salesDt > invoiceDt) {
+      return badRequest(
+        "Data de faturamento não pode ser menor que data da solicitação.",
+      );
+    }
+
+    if (addressHasUnpavedRoad && unpavedRoadSize === null) {
+      return badRequest("Km de estrada de chão não pode ser nulo.");
+    }
+
+    try {
+      const salesOrderOutputDto = await this.createSalesOrderRepository.execute(
+        httpRequest.body,
+      );
+
+      const createdSalesOrder: SalesOrder = {
+        id: salesOrderOutputDto.id,
+        salesDate: new Date(salesOrderOutputDto.salesDate),
+        invoiceDate: new Date(salesOrderOutputDto.invoiceDate),
+        user: {
+          id: salesOrderOutputDto.userId,
+          name: salesOrderOutputDto.userName,
+          email: salesOrderOutputDto.userEmail,
+          getFirstPriceRange: false,
+          managerName: salesOrderOutputDto.managerName,
+          managerEmail: salesOrderOutputDto.managerEmail,
+        },
+        customer: {
+          id: salesOrderOutputDto.customerId,
+          name: salesOrderOutputDto.customerName,
+          city: salesOrderOutputDto.customerCity,
+          state: salesOrderOutputDto.customerState,
+          paymentTerm: salesOrderOutputDto.customerPaymentTerm,
+        },
+        shipBase: salesOrderOutputDto.shipBase,
+        shipmentType: salesOrderOutputDto.shipmentType,
+        shippingCompanyName: salesOrderOutputDto.shippingCompanyName,
+        shippingCompanyContact: salesOrderOutputDto.shippingCompanyContact,
+        shippingCompanyPhone: salesOrderOutputDto.shippingCompanyPhone,
+        shippingCompanyEmail: salesOrderOutputDto.shippingCompanyEmail,
+        mapsLink: salesOrderOutputDto.mapsLink,
+        addressHasUnpavedRoad: salesOrderOutputDto.addressHasUnpavedRoad,
+        unpavedRoadSize: salesOrderOutputDto.unpavedRoadSize,
+        status: salesOrderOutputDto.status,
+      };
+
+      return created<SalesOrder>(createdSalesOrder);
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong!",
+      };
+    }
+  }
+}
